31cd85d7e00d5d7bf7f5005c47b94dc5ebedff55
Martin Wagner <martin.wagner@neuberger.net>
2017-05-09 16:28:23 +0200

    Added callback and query function for NMT state

--- stack/CO_NMT_Heartbeat.c
+++ stack/CO_NMT_Heartbeat.c
@@ -66,6 +66,7 @@ static void CO_NMT_receive(void *object, const CO_CANrxMsg_t *msg){
 
     if((msg->DLC == 2) && ((nodeId == 0) || (nodeId == NMT->nodeId))){
         uint8_t command = msg->data[0];
+        uint8_t currentOperatingState = NMT->operatingState;
 
         switch(command){
             case CO_NMT_ENTER_OPERATIONAL:
@@ -85,6 +86,10 @@ static void CO_NMT_receive(void *object, const CO_CANrxMsg_t *msg){
             case CO_NMT_RESET_COMMUNICATION:
                 NMT->resetCommand = CO_RESET_COMM;
                 break;
+        }
+
+        if(NMT->pFunctNMT!=NULL && currentOperatingState!= NMT->operatingState){
+            NMT->pFunctNMT(NMT->operatingState);
         }
     }
 }
@@ -147,6 +152,17 @@ CO_ReturnError_t CO_NMT_init(
             0);                 /* synchronous message flag bit */
 
     return CO_ERROR_NO;
+}
+
+
+/******************************************************************************/
+void CO_NMT_initCallback(
+        CO_NMT_t               *NMT,
+        void                  (*pFunctNMT)(CO_NMT_internalState_t state))
+{
+    if(NMT != NULL){
+        NMT->pFunctNMT = pFunctNMT;
+    }
 }
 
 
@@ -319,3 +335,15 @@ CO_NMT_reset_cmd_t CO_NMT_process(
 
     return NMT->resetCommand;
 }
+
+
+/******************************************************************************/
+CO_NMT_internalState_t CO_NMT_getInternalState(
+        CO_NMT_t               *NMT)
+{
+    if(NMT != NULL){
+        return NMT->operatingState;
+    }
+    return CO_NMT_INITIALIZING;
+}
+
