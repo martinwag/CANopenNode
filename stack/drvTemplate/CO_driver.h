e0751435dd37d9356159fbb37c3568c05f6c4ab9
Martin Wagner <martin.wagner@neuberger.net>
2017-12-15 10:36:43 +0100

    extended driver template (new errors, CO_CANcheckSend() function)

--- stack/drvTemplate/CO_driver.h
+++ stack/drvTemplate/CO_driver.h
@@ -225,21 +225,23 @@ extern "C" {
  * successfully it returns 0 otherwise it returns <0.
  */
 typedef enum{
-    CO_ERROR_NO                 = 0,    /**< Operation completed successfully */
-    CO_ERROR_ILLEGAL_ARGUMENT   = -1,   /**< Error in function arguments */
-    CO_ERROR_OUT_OF_MEMORY      = -2,   /**< Memory allocation failed */
-    CO_ERROR_TIMEOUT            = -3,   /**< Function timeout */
-    CO_ERROR_ILLEGAL_BAUDRATE   = -4,   /**< Illegal baudrate passed to function CO_CANmodule_init() */
-    CO_ERROR_RX_OVERFLOW        = -5,   /**< Previous message was not processed yet */
-    CO_ERROR_RX_PDO_OVERFLOW    = -6,   /**< previous PDO was not processed yet */
-    CO_ERROR_RX_MSG_LENGTH      = -7,   /**< Wrong receive message length */
-    CO_ERROR_RX_PDO_LENGTH      = -8,   /**< Wrong receive PDO length */
-    CO_ERROR_TX_OVERFLOW        = -9,   /**< Previous message is still waiting, buffer full */
-    CO_ERROR_TX_PDO_WINDOW      = -10,  /**< Synchronous TPDO is outside window */
-    CO_ERROR_TX_UNCONFIGURED    = -11,  /**< Transmit buffer was not confugured properly */
-    CO_ERROR_PARAMETERS         = -12,  /**< Error in function function parameters */
-    CO_ERROR_DATA_CORRUPT       = -13,  /**< Stored data are corrupt */
-    CO_ERROR_CRC                = -14   /**< CRC does not match */
+  CO_ERROR_NO                 = 0,    /**< Operation completed successfully */
+  CO_ERROR_ILLEGAL_ARGUMENT   = -1,   /**< Error in function arguments */
+  CO_ERROR_OUT_OF_MEMORY      = -2,   /**< Memory allocation failed */
+  CO_ERROR_TIMEOUT            = -3,   /**< Function timeout */
+  CO_ERROR_ILLEGAL_BAUDRATE   = -4,   /**< Illegal baudrate passed to function CO_CANmodule_init() */
+  CO_ERROR_RX_OVERFLOW        = -5,   /**< Previous message was not processed yet */
+  CO_ERROR_RX_PDO_OVERFLOW    = -6,   /**< previous PDO was not processed yet */
+  CO_ERROR_RX_MSG_LENGTH      = -7,   /**< Wrong receive message length */
+  CO_ERROR_RX_PDO_LENGTH      = -8,   /**< Wrong receive PDO length */
+  CO_ERROR_TX_OVERFLOW        = -9,   /**< Previous message is still waiting, buffer full */
+  CO_ERROR_TX_BUSY            = -10,  /**< Sending rejected because driver is busy. Try again */
+  CO_ERROR_TX_PDO_WINDOW      = -11,  /**< Synchronous TPDO is outside window */
+  CO_ERROR_TX_UNCONFIGURED    = -12,  /**< Transmit buffer was not confugured properly */
+  CO_ERROR_PARAMETERS         = -13,  /**< Error in function function parameters */
+  CO_ERROR_DATA_CORRUPT       = -14,  /**< Stored data are corrupt */
+  CO_ERROR_CRC                = -15,  /**< CRC does not match */
+  CO_ERROR_WRONG_NMT_STATE    = -16   /**< Command can't be processed in current state */
 }CO_ReturnError_t;
 
 
@@ -448,6 +450,24 @@ CO_CANtx_t *CO_CANtxBufferInit(
  * CO_ERROR_TX_PDO_WINDOW (Synchronous TPDO is outside window).
  */
 CO_ReturnError_t CO_CANsend(CO_CANmodule_t *CANmodule, CO_CANtx_t *buffer);
+
+
+/**
+ * The same as #CO_CANsend(), but ensures that there is enough space remaining
+ * in the driver for more important messages.
+ *
+ * The remaining amount depends on the implementation. It is at least 1 message
+ * buffer. If sending would violate those limits, #CO_ERROR_TX_BUSY is returned
+ * and the message will not be sent.
+ *
+ * @param CANmodule This object.
+ * @param buffer Pointer to transmit buffer, returned by CO_CANtxBufferInit().
+ * Data bytes must be written in buffer before function call.
+ *
+ * @return #CO_ReturnError_t: CO_ERROR_NO, CO_ERROR_TX_OVERFLOW or
+ * CO_ERROR_TX_PDO_WINDOW (Synchronous TPDO is outside window).
+ */
+CO_ReturnError_t CO_CANCheckSend(CO_CANmodule_t *CANmodule, CO_CANtx_t *buffer);
 
 
 /**
